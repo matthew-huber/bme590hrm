bb55127270ce59cea3c4c88104139372be57a7d9
Matthew Huber <matthew.huber@duke.edu>
2018-10-20 09:40:05 -0400

    added find_duration function which takes time array and finds maximum value. create_and_fill_dict calls this function to replace the duration dictionary value

--- analyze_data.py
+++ analyze_data.py
@@ -2,11 +2,17 @@
 
 def create_and_fill_dict(times, voltage):
 
-    data_dict = {"duration": -1,
+    data_dict = {"duration": '',
                  "bpm": -1,
                  "beat times": -1,
                  "min max": -1,
                  "num beats": -1
                  }
 
+    data_dict["duration"] = find_duration(times)
+
     return data_dict
+
+def find_duration(times):
+    max_time = max(times)
+    return max_time
