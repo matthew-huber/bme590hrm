5f77dc48d79ec64190f1c7e1c94ae5cb0668dd77
Matthew Huber <matthew.huber@duke.edu>
2018-10-20 09:34:37 -0400

    added dictionary to hold data analysis results. All parameters initialized to -1.

--- analyze_data.py
+++ analyze_data.py
@@ -1,1 +1,12 @@
-:ww
+
+
+def create_and_fill_dict(times, voltage):
+
+    data_dict = {"duration": -1,
+                 "bpm": -1,
+                 "beat times": -1,
+                 "min max": -1,
+                 "num beats": -1
+                 }
+
+    return data_dict
