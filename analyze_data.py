1184c87857ee7f9b57e262a61912fcb4c9fc782a
Matthew Huber <matthew.huber@duke.edu>
2018-10-22 17:43:22 -0400

    added find_beat_times, returns list of beat times
    Two functions which have yet to be written are called in this function:
    beat_in_interval: which takes in a time and voltage array, an interval, and an interval end time and returns the time that a beat occured in that interval.
    combine_double_beats: if the beat_times array finds two beats close in
    time, the higher amplitude time is used for the heartbeat.

--- analyze_data.py
+++ analyze_data.py
@@ -11,6 +11,7 @@ def create_and_fill_dict(times, voltage):
 
     data_dict["duration"] = find_duration(times)
     data_dict["min max"] = min_max_voltage(voltage)
+    data_dict["beat tiems"] = find_duration(times, voltage)
     return data_dict
 
 
@@ -24,3 +25,15 @@ def min_max_voltage(voltage):
     max_voltage = max(voltage)
     min_max_voltages = (min_voltage, max_voltage)
     return min_max_voltages
+
+def find_beat_times(times, voltage):
+    interval = 0.5
+    threshold = 0.75
+
+    beat_times = []
+    for time in range(interval, max(times), interval):
+        beat_times = beat_times.append(beat_in_interval(times, voltage, times))
+
+    beat_times = combine_double_beats(times, voltage, beat_times)
+    return beat_times
+
