ebaef4659736bc8fcc9f456c64278a5cf843ed66
Matthew Huber <matthew.huber@duke.edu>
2018-10-28 12:29:25 -0400

    basic test of find_bpm

---
+++ test_find_bpm.py
@@ -0,0 +1,15 @@
+
+import pytest
+
+from analyze_data import find_bpm
+from fileReader import load_csv
+
+times, voltages = load_csv('./test_data/test_data1.csv')
+
+
+@pytest.mark.parametrize("beat_times, range, expected", [
+    ([0, 10, 20], (0, 20), 9)
+])
+def test_find_bpm(beat_times, range, expected):
+    beat_times = find_bpm(beat_times, range)
+    assert beat_times == pytest.approx(expected)
